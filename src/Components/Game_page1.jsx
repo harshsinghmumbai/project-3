60a015cd8af98513eb7432ff93cca2bb01baaa01
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-11-07 16:45:04 +0530

    images

--- src/Components/Game_page1.jsx
+++ src/Components/Game_page1.jsx
@@ -1,9 +1,14 @@
 const Game_page1 = () => {
-    return <>
-        <div className="border-2 border-black w-[80%] h-80">
-
+  return (
+    <>
+      <div className="border-2 border-black w-[80%] h-80">
+        <div className="w-3/6">
+          <img src="./src/assets" alt="image" />
         </div>
-    </>;
+        <div className="w-3/6"></div>
+      </div>
+    </>
+  );
 };
 
 export default Game_page1;
