6546eab8b65bfa57f4adf06eefbb97e7bdb7b539
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-11-07 17:09:10 +0530

    image update

--- src/Components/Game_page1.jsx
+++ src/Components/Game_page1.jsx
@@ -3,9 +3,18 @@ const Game_page1 = () => {
     <>
       <div className="border-2 border-black w-[80%] h-80">
         <div className="w-3/6">
-          <img src="./src/assets" alt="image" />
+          <img
+            src="./src/assets/dices 1.jpg"
+            className="h-[19.8rem] w-full object-cover"
+            alt="image"
+          />
         </div>
-        <div className="w-3/6"></div>
+        <div className="w-3/6">
+          <p className="w-full">
+            <h1 className="text-5xl font-bold">dice game</h1>
+            <button className="bg-black text-white">Play now</button>
+          </p>
+        </div>
       </div>
     </>
   );
