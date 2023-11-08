e0cd57525083e06be2ba87ae7449176456b72a2a
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-11-08 18:51:33 +0530

    usesatete use

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Game_page1 from "./Components/Game_Play";
 
 const App = () => {
+  const [isGamestarted, setisGamestarted] = useState(flase);
   return (
     <>
       <main className="flex justify-center items-center h-screen">
