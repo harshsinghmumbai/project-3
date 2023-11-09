cea0de3a4a141f3af41bcf0729b2f1b6c1755269
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-11-09 17:00:43 +0530

    startgame

--- src/Components/StartGame.jsx
+++ src/Components/StartGame.jsx
@@ -1,7 +1,13 @@
 const StartGame = () => {
   return (
     <>
-      <h1>game start</h1>
+      <div className="border-2 border-black mt-7 w-[]">
+        <div className="border-2 border-black w-[5rem] h-[6rem]">
+          <h1 className="text-5xl font-normal">0</h1>
+          <p className="w-full text-lg">TotalScore</p>
+        </div>
+        <div className=""></div>
+      </div>
     </>
   );
 };
