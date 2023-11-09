5551e0bed006275d1aa0ce662ffd71b65be6eabc
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-11-09 17:03:32 +0530

    change

--- src/Components/StartGame.jsx
+++ src/Components/StartGame.jsx
@@ -1,10 +1,12 @@
 const StartGame = () => {
   return (
     <>
-      <div className="border-2 border-black mt-7 w-[]">
+      <div className="border-2 border-black mt-7 w-[95%] m-auto">
+
+        {/* ----------------------Score Area-------------------- */}
         <div className="border-2 border-black w-[5rem] h-[6rem]">
-          <h1 className="text-5xl font-normal">0</h1>
-          <p className="w-full text-lg">TotalScore</p>
+          <h1 className="text-5xl font-normal inline-block m-auto">0</h1>
+          <p className="w-full text-base">TotalScore</p>
         </div>
         <div className=""></div>
       </div>
