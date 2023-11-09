e65ecf89a5d898cb0c5391f7207719b3f64aa38c
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-11-09 17:53:29 +0530

    update

--- src/Components/StartGame.jsx
+++ src/Components/StartGame.jsx
@@ -1,8 +1,7 @@
 const StartGame = () => {
   return (
     <>
-      <div className="border-2 border-black mt-7 w-[95%] m-auto flex justify-between">
-
+      <div className="mt-7 w-[95%] m-auto flex justify-between">
         {/* ---------------------Total Score-------------------- */}
         <div className="w-[7rem] h-[6rem] p-1">
           <h1 className="text-6xl font-[400] inline-block ml-6">0</h1>
@@ -10,8 +9,15 @@ const StartGame = () => {
         </div>
 
         {/* ----------------------Select Number----------------------- */}
-        <div className="border-2 border-black h-full w-28">
+        <div className="w-[18rem] space-x-4 mt-2 ">
+          <p className="border-[1px] border-black p-1 pl-[0.6rem]  w-8 h-8 inline-block">1</p>
+          <p className="border-[1px] border-black p-1 pl-[0.6rem]  w-8 h-8 inline-block">2</p>
+          <p className="border-[1px] border-black p-1 pl-[0.6rem]  w-8 h-8 inline-block">3</p>
+          <p className="border-[1px] border-black p-1 pl-[0.6rem]  w-8 h-8 inline-block">4</p>
+          <p className="border-[1px] border-black p-1 pl-[0.6rem]  w-8 h-8 inline-block">5</p>
+          <p className="border-[1px] border-black p-1 pl-[0.6rem]  w-8 h-8 inline-block">6</p>
 
+          <p className="flex justify-end mt-6 font-semibold mr-[1rem]">Select Number</p>
         </div>
       </div>
     </>
